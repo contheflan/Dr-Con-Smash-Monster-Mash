7421f1a84cc58e0c48dbad5a4a62718c98432116
Connor McKenna <connormckenna@Connors-MacBook-Pro.local>
2020-10-15 16:15:32 -0400

    removed results file, added more styling to monsters page

--- monster-mash/src/components/Monsters.jsx
+++ monster-mash/src/components/Monsters.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Monsters = () => {
-  const [creations, setCreations] = useState([])
+  const [creations, setCreations] = useState([]);
   useEffect(() => {
     const getCreations = async () => {
       const airtableURL = `${baseURL}/creations`;
@@ -18,18 +18,22 @@ const Monsters = () => {
     getCreations();
   }, []);
   return (
-    <>
-    <Link style={{ textDecoration: "none" }} className="Back" to="/">
-    Back
-  </Link>
-
-    <div>
-      {creations.map((creations) => (
-          <p>{creations.fields.Name}</p>
-        
-      ))}
+    <div className="Results-container">
+      <Link style={{ textDecoration: "none" }} className="Results-back" to="/">
+        Back
+      </Link>
+      <div>
+        <p className="Intro-text">THE GOBLIN HALL OF FAME OR WHATEVER</p>
+        {creations.map((creations) => (
+          <p className="Results">
+            {creations.fields.Name} STR: {creations.fields.STR} DEX:
+            {creations.fields.DEX} CON: {creations.fields.CON} INT:
+            {creations.fields.INT} WIS: {creations.fields.WIS} CHA:
+            {creations.fields.CHA}
+          </p>
+        ))}
       </div>
-      </>
+    </div>
   );
 };
 
